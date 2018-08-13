5fef408532ed51f7fb54bdbc466ac805ba11272c
ravi <ravi@ravi-PC>
2018-08-13 20:50:18 +0530

    logo V2

--- application/views/accountant/_head_accountant.php
+++ application/views/accountant/_head_accountant.php
@@ -3,10 +3,8 @@
 </div>
 <div class="social-links">
     <ul>
-        <li><a href="#">
-            <?php echo $this->session->userdata('userbean')->first_name ?>
-            <?php echo $this->session->userdata('userbean')->last_name ?>
-            </a></li>
+        <li><a href="#"><?php echo $this->session->userdata('userbean')->first_name ?>
+            <?php echo $this->session->userdata('userbean')->last_name ?></a></li>
         <li>[<?php echo $this->session->userdata('userbean')->user_role ?>]</li>
         <li><a href="<?php echo base_url('User_Controller/logoutHMS');?>">Logout</a></li>
         <div class="clear"> </div>
