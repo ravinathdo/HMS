703159e5b8e67c4f5e61b99b390da41d0b24a7f3
ravi <ravi@ravi-PC>
2018-08-30 08:54:37 +0530

    opd , accountant done

--- application/views/accountant/_tree_accountant.php
+++ application/views/accountant/_tree_accountant.php
@@ -1,22 +1,10 @@
 <ul style="padding-left: 5px">
-    <li><a href="<?php echo base_url('Accountant_Controller/loadPurchasingItem'); ?>">
-            <img src="<?= base_url('/images/icon-purchasing.png') ?>" alt="..." class="img-thumbnail menu-icon" title="Purchasing Items"> Purchasing Items
-        </a></li>
-    <li><a href="<?php echo base_url('Accountant_Controller/loadEmployeeSalary'); ?>">
-            <img src="<?= base_url('/images/icon-employee.png') ?>" alt="..." class="img-thumbnail  menu-icon" title="Employee Salary"> Employee Salary
-        </a></li>
-    <li><a href="<?php echo base_url('Accountant_Controller/loaddoctorSalary'); ?>">
-            <img src="<?= base_url('/images/icon-doctor.png') ?>" alt="..." class="img-thumbnail  menu-icon" title="Doctor Salary"> Doctor Salary
-        </a></li>
-    <li><a href="<?php echo base_url('Accountant_Controller/loadCostMaintain'); ?>">
-            <img src="<?= base_url('/images/icon-cost.png') ?>" alt="..." class="img-thumbnail  menu-icon" title="Cost"> Cost
-        </a></li>
-    <li><a href="<?php echo base_url('Transport_Controller/#'); ?>">
-            <img src="<?= base_url('/images/icon-report.png') ?>"  alt="..." class="img-thumbnail menu-icon" title="Ambulance Request"> Reports
-        </a></li>
-    <li><a href="<?php echo base_url('User_Controller/loadProfile'); ?>">
-            <img src="<?= base_url('/images/icon-profile.png') ?>"  alt="..." class="img-thumbnail menu-icon" title="Ambulance Request"> Profile
-        </a></li>
+    <li><tt><pre><a href="<?php echo base_url('Accountant_Controller/loadPurchasingItem'); ?>"><i class="fas fa-flask "></i> Purchasing Items</a></pre></tt></li>
+    <li><tt><pre><a href="<?php echo base_url('Accountant_Controller/loadEmployeeSalary'); ?>"><i class="fas fa-users "></i>Employee Salary</a></pre></tt></li>
+    <li><tt><pre><a href="<?php echo base_url('Accountant_Controller/loaddoctorSalary'); ?>"><i class="fas fa-user-md "></i> Doctor Salary</a></pre></tt></li>
+    <li><tt><pre><a href="<?php echo base_url('Accountant_Controller/loadCostMaintain'); ?>"><i class="fas fa-money-bill-alt "></i> Cost</a></pre></tt></li>
+    <li><tt><pre><a href="<?php echo base_url('Transport_Controller/#'); ?>"></a> Reports</pre></tt></li>
+    <li><tt><pre><a href="<?php echo base_url('User_Controller/loadProfile'); ?>"><i class="fas fa-user-cog "></i> Profile</a></pre></tt></li>
 </ul>
 
 
--- application/views/accountant/home.php
+++ application/views/accountant/home.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="<?php echo base_url('css/responsiveslides.css') ?>">
         <script src="<?php echo base_url('js/jquery.min.js'); ?>"></script>		
         <script src="<?php echo base_url('js/responsiveslides.min.js'); ?>"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
         <script>
             // You can also use "$(window).load(function() {"
@@ -43,38 +44,62 @@
         <!---End-header---->
 
         <!----start-content----->
-        <div class="content">
+        <div class="content" style="min-height: 400px">
             <div class="row">
                 <div class="col-md-2"> 
                     <?php $this->load->view('accountant/_tree_accountant'); ?>
                 </div>
-                <div class="col-md-10">
+                <div class="col-md-8">
                     <h2>Accountant</h2>
-                    <a href="<?php echo base_url('Accountant_Controller/loadPurchasingItem'); ?>">
-                        <img src="<?= base_url('/images/icon-purchasing.png') ?>" alt="..." class="img-thumbnail tile-icon" title="Purchasing Items">Purchase Items
-                    </a>
-                    <a href="<?php echo base_url('Accountant_Controller/loadEmployeeSalary'); ?>">
-                        <img src="<?= base_url('/images/icon-employee.png') ?>" alt="..." class="img-thumbnail  tile-icon" title="">Employee Salary
-                    </a>
-                    <a href="<?php echo base_url('Accountant_Controller/loaddoctorSalary'); ?>">
-                        <img src="<?= base_url('/images/icon-doctor.png') ?>" alt="..." class="img-thumbnail  tile-icon" title="">Doctor Payments
-                    </a>
-                    <a href="<?php echo base_url('Accountant_Controller/loadCostMaintain'); ?>">
-                        <img src="<?= base_url('/images/icon-cost.png') ?>" alt="..." class="img-thumbnail  tile-icon" title="View Stock">
-                    </a>
-                    <a href="<?php echo base_url('#'); ?>">
-                        <i class="fas fa-address-book   fa-5x img-thumbnail tile-icon"></i> Reports
-                    </a>
-                    <a href="<?php echo base_url('User_Controller/loadProfile'); ?>">
-                        <i class="fas fa-user-cog fa-5x img-thumbnail tile-icon"></i> Profile
-                    </a>
+                    <table border="0" style="width: 100%">
+                        
+                        <tbody>
+                            <tr style="text-align: center">
+                                <td><a href="<?php echo base_url('Accountant_Controller/loadPurchasingItem'); ?>">
+                                        <i class="fas fa-flask fa-5x"></i> 
+                                    </a></td>
+                                <td><a href="<?php echo base_url('Accountant_Controller/loadEmployeeSalary'); ?>">
+                                        <i class="fas fa-users fa-5x"></i>
+                                    </a></td>
+                                <td><a href="<?php echo base_url('Accountant_Controller/loaddoctorSalary'); ?>">
+                                        <i class="fas fa-user-md fa-5x"></i>
+                                    </a></td>
+                                <td><a href="<?php echo base_url('Accountant_Controller/loadCostMaintain'); ?>">
+                                        <i class="fas fa-money-bill-alt fa-5x"></i>
+                                    </a></td>
+                                <td><a href="<?php echo base_url('#'); ?>">
+                                        <i class="fas fa-address-book   fa-5x"></i>
+                                    </a></td>
+                                <td><a href="<?php echo base_url('User_Controller/loadProfile'); ?>">
+                                        <i class="fas fa-user-cog fa-5x"></i> 
+                                    </a></td>
+                            </tr>
+                            <tr style="text-align: center">
+                                <td>Purchase Items</td>
+                                <td>Employee Salary</td>
+                                <td>Doctor Payments</td>
+                                <td>Cost Management</td>
+                                <td>Reports</td>
+                                <td>Profile</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+
+
+
+
+
+
+
                 </div>
+                <div class="col-md-4"></div>
             </div>
         </div>
         <!----End-content----->
         <!---End-wrap---->
         <!---start-footer---->
-              <?php $this->load->view('_footer'); ?>
+        <?php $this->load->view('_footer'); ?>
         <!---End-footer---->
     </body>
 </html>
