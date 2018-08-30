f2a9bb2e24ef3cd50af3b2a82d9da07ab75a0ed1
User <User@DESKTOP-76HRC5D>
2018-08-30 22:42:18 +0530

    space on home , db empty

---
+++ hmsdb_empty.sql
@@ -0,0 +1,376 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.5.54 : Database - hmsdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hmsdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `hmsdb`;
+
+/*Table structure for table `hms_center` */
+
+DROP TABLE IF EXISTS `hms_center`;
+
+CREATE TABLE `hms_center` (
+  `center_name` varchar(20) NOT NULL,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NewIndex1` (`center_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_center` */
+
+insert  into `hms_center`(`center_name`,`id`) values ('Colombo',1);
+
+/*Table structure for table `hms_cost` */
+
+DROP TABLE IF EXISTS `hms_cost`;
+
+CREATE TABLE `hms_cost` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `description` varchar(100) DEFAULT NULL,
+  `amount` double(5,2) DEFAULT NULL,
+  `txn_type` varchar(20) DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_cost` */
+
+/*Table structure for table `hms_doctor` */
+
+DROP TABLE IF EXISTS `hms_doctor`;
+
+CREATE TABLE `hms_doctor` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `nic` varchar(12) DEFAULT NULL,
+  `pword` text,
+  `email` varchar(50) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `degree` varchar(200) DEFAULT NULL,
+  `specialist_id` int(5) DEFAULT NULL,
+  `doc_fee` int(5) DEFAULT NULL,
+  `slmc_no` varchar(20) DEFAULT NULL,
+  `status_code` varchar(20) DEFAULT 'ACTIVE',
+  `category` varchar(20) DEFAULT 'OPD',
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  `user_role` varchar(20) DEFAULT 'DOCTOR',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NewIndex1` (`nic`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_doctor` */
+
+/*Table structure for table `hms_doctor_appointment` */
+
+DROP TABLE IF EXISTS `hms_doctor_appointment`;
+
+CREATE TABLE `hms_doctor_appointment` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `doctor_id` int(5) DEFAULT NULL,
+  `patient_id` int(5) DEFAULT NULL,
+  `appointment_date` varchar(30) DEFAULT NULL,
+  `status_code` varchar(20) DEFAULT 'OPEN',
+  `doctor_comment` text,
+  `doctor_fee` int(5) DEFAULT NULL,
+  `hospital_fee` int(5) DEFAULT NULL,
+  `fee` int(5) DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_doctor_appointment` */
+
+/*Table structure for table `hms_doctor_availability` */
+
+DROP TABLE IF EXISTS `hms_doctor_availability`;
+
+CREATE TABLE `hms_doctor_availability` (
+  `doctor_id` int(5) NOT NULL,
+  `day_available` varchar(60) NOT NULL,
+  `time_available` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`doctor_id`,`day_available`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_doctor_availability` */
+
+/*Table structure for table `hms_drug` */
+
+DROP TABLE IF EXISTS `hms_drug`;
+
+CREATE TABLE `hms_drug` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `drug_name` varchar(50) DEFAULT NULL,
+  `qty` int(5) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_drug` */
+
+/*Table structure for table `hms_employee_salary` */
+
+DROP TABLE IF EXISTS `hms_employee_salary`;
+
+CREATE TABLE `hms_employee_salary` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `user_id` int(5) DEFAULT NULL,
+  `salary_month` varchar(20) DEFAULT NULL,
+  `salary_amount` varbinary(10) DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NewIndex1` (`user_id`,`salary_month`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_employee_salary` */
+
+/*Table structure for table `hms_feedback` */
+
+DROP TABLE IF EXISTS `hms_feedback`;
+
+CREATE TABLE `hms_feedback` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `feedback` text,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_feedback` */
+
+/*Table structure for table `hms_inventory` */
+
+DROP TABLE IF EXISTS `hms_inventory`;
+
+CREATE TABLE `hms_inventory` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(50) DEFAULT NULL,
+  `qty` int(5) DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  `updated_date` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_inventory` */
+
+/*Table structure for table `hms_lab_test` */
+
+DROP TABLE IF EXISTS `hms_lab_test`;
+
+CREATE TABLE `hms_lab_test` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `lab_test` varchar(50) DEFAULT NULL,
+  `center_name` varchar(20) DEFAULT NULL,
+  `description` text,
+  `test_cost` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_lab_test` */
+
+/*Table structure for table `hms_opd_appointment` */
+
+DROP TABLE IF EXISTS `hms_opd_appointment`;
+
+CREATE TABLE `hms_opd_appointment` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `patient_id` int(5) DEFAULT NULL,
+  `doctor_id` int(5) DEFAULT NULL,
+  `appointment_date` varchar(30) DEFAULT NULL,
+  `status_code` varchar(20) DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  `updated_user` int(5) DEFAULT NULL,
+  `fee` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_opd_appointment` */
+
+/*Table structure for table `hms_patient` */
+
+DROP TABLE IF EXISTS `hms_patient`;
+
+CREATE TABLE `hms_patient` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `dob` varchar(25) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `pword` text,
+  `status_code` varchar(20) DEFAULT 'ACTIVE',
+  `user_role` varchar(20) DEFAULT 'PATIENT',
+  `created_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NewIndex1_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_patient` */
+
+/*Table structure for table `hms_purchase` */
+
+DROP TABLE IF EXISTS `hms_purchase`;
+
+CREATE TABLE `hms_purchase` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `purchasing_item` varchar(50) DEFAULT NULL,
+  `qty` int(5) DEFAULT NULL,
+  `status_code` varchar(20) DEFAULT 'PENDING',
+  `request_by` int(5) DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  `amount` double(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_purchase` */
+
+/*Table structure for table `hms_specialist` */
+
+DROP TABLE IF EXISTS `hms_specialist`;
+
+CREATE TABLE `hms_specialist` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `specialist` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_specialist` */
+
+/*Table structure for table `hms_user` */
+
+DROP TABLE IF EXISTS `hms_user`;
+
+CREATE TABLE `hms_user` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `nic` varchar(12) DEFAULT NULL,
+  `pword` text,
+  `user_role` varchar(20) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `empno` varchar(10) DEFAULT NULL,
+  `status_code` varchar(20) DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NewIndex1` (`nic`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_user` */
+
+insert  into `hms_user`(`id`,`first_name`,`last_name`,`nic`,`pword`,`user_role`,`telephone`,`email`,`empno`,`status_code`,`created_date`,`created_user`) values (1,'kumara','pathirana','88','7c4a8d09ca3762af61e59520943dc26494f8941b','ADMIN','0111','admin@gmail.com','2255','ACTIVE','2018-07-27 21:20:34',1),(2,'Shalinqx','Tha','89','7c4a8d09ca3762af61e59520943dc26494f8941b','OPD','02244','','2266','ACTIVE','2018-08-02 12:01:43',1),(3,'Gayaxxs','Sirix','90','7c4a8d09ca3762af61e59520943dc26494f8941b','ACCOUNTANT','','','2267','ACTIVE','2018-08-06 12:04:25',1),(5,'Nuwax','Perd','92','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','LAB','dd@g.com','','2269','ACTIVE','2018-08-06 12:06:43',1),(6,'KK','ii','93','7c4a8d09ca3762af61e59520943dc26494f8941b','TRANSPORT','011','ravi@gm','2270','ACTIVE','2018-08-06 16:03:16',1),(7,'kx','lklklxxs','94','7c4a8d09ca3762af61e59520943dc26494f8941b','PHARMACIST','055','dads@gm.l','6633','ACTIVE','2018-08-07 12:38:30',1),(8,'Rasintha','kjkjk','95','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','WARD','07155544','',NULL,'ACTIVE','2018-08-07 16:23:25',1);
+
+/*Table structure for table `hms_user_role` */
+
+DROP TABLE IF EXISTS `hms_user_role`;
+
+CREATE TABLE `hms_user_role` (
+  `user_role` varchar(20) NOT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`user_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_user_role` */
+
+insert  into `hms_user_role`(`user_role`,`description`) values ('ACCOUNTANT','Accountant'),('ADMIN','Administrator'),('DOCTOR','Doctor'),('HR','HR Manager'),('LAB','LAB Inchage'),('OPD','OPD User'),('PATIENT','Patient'),('PHARMACIST','Pharmacist'),('TRANSPORT','Transport Manager'),('WARD','Ward Management');
+
+/*Table structure for table `hms_vehicle` */
+
+DROP TABLE IF EXISTS `hms_vehicle`;
+
+CREATE TABLE `hms_vehicle` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `vehicle_number` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NewIndex1` (`vehicle_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_vehicle` */
+
+/*Table structure for table `hms_vehicle_request` */
+
+DROP TABLE IF EXISTS `hms_vehicle_request`;
+
+CREATE TABLE `hms_vehicle_request` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `request_by` int(5) DEFAULT NULL,
+  `comment` text,
+  `status_code` varchar(20) DEFAULT 'PENDING',
+  `vehicle_id` int(5) DEFAULT '0',
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  `datetime_need` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_vehicle_request` */
+
+/*Table structure for table `hms_ward` */
+
+DROP TABLE IF EXISTS `hms_ward`;
+
+CREATE TABLE `hms_ward` (
+  `ward_no` int(5) NOT NULL AUTO_INCREMENT,
+  `ward_name` varchar(50) DEFAULT NULL,
+  `doctor_incharge` int(5) DEFAULT NULL,
+  PRIMARY KEY (`ward_no`),
+  UNIQUE KEY `NewIndex1` (`ward_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_ward` */
+
+/*Table structure for table `hms_ward_patient` */
+
+DROP TABLE IF EXISTS `hms_ward_patient`;
+
+CREATE TABLE `hms_ward_patient` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `ward_id` int(5) DEFAULT NULL,
+  `patient_id` int(5) DEFAULT NULL,
+  `comment` varchar(250) DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user` int(5) DEFAULT NULL,
+  `status_code` varchar(20) DEFAULT 'ADMIT',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NewIndex1` (`ward_id`,`patient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_ward_patient` */
+
+/*Table structure for table `hms_ward_staff` */
+
+DROP TABLE IF EXISTS `hms_ward_staff`;
+
+CREATE TABLE `hms_ward_staff` (
+  `ward_id` int(5) NOT NULL,
+  `staff_id` int(5) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `responsibility` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`ward_id`,`staff_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hms_ward_staff` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
