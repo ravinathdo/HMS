658aa98a228b2ea2cf77ccfb894f51a2346f3814
ravi <ravi@ravi-PC>
2018-08-30 10:51:07 +0530

    driver manage

--- application/views/transport/_tree_transport.php
+++ application/views/transport/_tree_transport.php
@@ -2,6 +2,7 @@
     <li><tt><pre><a href="<?php echo base_url('Transport_Controller/loadAmbulance'); ?>"><i class="fas fa-ambulance "></i> Ambulance Manage</a></pre></tt></li>
     <li><tt><pre><a href="<?php echo base_url('Transport_Controller/loadAmbulanceRequestList'); ?>"><i class="fas fa-headset "></i> Ambulance Request</a></pre></tt></li>
     <li><tt><pre><a href="<?php echo base_url('Transport_Controller/getPendingVehicleRequest'); ?>"><i class="fas fa-address-book    "></i> Pending Request</a></pre></tt></li>
+    <li><tt><pre><a href="<?php echo base_url('Transport_Controller/#'); ?>"><i class="fas fa-bezier-curve"></i> Manage Drivers</a></pre></tt></li>
     <li><tt><pre><a href="<?php echo base_url('Transport_Controller/#'); ?>"><i class="fas fa-user-cog "></i> Reports</a></pre></tt></li>
     <li><tt><pre><a href="<?php echo base_url('User_Controller/loadProfile'); ?>"><i class="fab fa-algolia "></i> Profile</a></pre></tt></li>
 </ul>
