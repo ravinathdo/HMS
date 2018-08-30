f2ec088f1618d6383d2e51117d96ed2e80fd5114
ravi <ravi@ravi-PC>
2018-08-30 10:42:26 +0530

    lab test cost done | update the database 30082018_2

--- hmsdb_30082018_2.sql
+++ hmsdb_30082018_2.sql
@@ -41,9 +41,11 @@ CREATE TABLE `hms_cost` (
   `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `created_user` int(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_cost` */
+
+insert  into `hms_cost`(`id`,`description`,`amount`,`txn_type`,`created_date`,`created_user`) values (1,'asdasd',222.00,'Credit','2018-08-28 21:01:21',5);
 
 /*Table structure for table `hms_doctor` */
 
@@ -68,9 +70,11 @@ CREATE TABLE `hms_doctor` (
   `user_role` varchar(20) DEFAULT 'DOCTOR',
   PRIMARY KEY (`id`),
   UNIQUE KEY `NewIndex1` (`nic`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_doctor` */
+
+insert  into `hms_doctor`(`id`,`first_name`,`last_name`,`nic`,`pword`,`email`,`telephone`,`degree`,`specialist_id`,`doc_fee`,`slmc_no`,`status_code`,`category`,`created_date`,`created_user`,`user_role`) values (1,'Sampath','Perera','863512824V','b24777061756485c930c12fba0d84d37ab356646','sampa@gmail.com','0715833470','MBBS',1,2500,'5566','ACTIVE',NULL,'2018-08-29 14:11:29',1,'DOCTOR');
 
 /*Table structure for table `hms_doctor_appointment` */
 
@@ -89,9 +93,11 @@ CREATE TABLE `hms_doctor_appointment` (
   `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `created_user` int(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_doctor_appointment` */
+
+insert  into `hms_doctor_appointment`(`id`,`doctor_id`,`patient_id`,`appointment_date`,`status_code`,`doctor_comment`,`doctor_fee`,`hospital_fee`,`fee`,`created_date`,`created_user`) values (1,1,1,'2018-08-31','COMPLETE',' Feever record available',2500,1000,3500,'2018-08-29 14:12:36',1),(2,1,1,'2018-08-31','COMPLETE',' this is good',2500,1000,3500,'2018-08-29 16:27:14',1),(3,1,1,'2018-09-01','COMPLETE',' sadsadsadsa',2500,1000,3500,'2018-08-29 16:30:14',1);
 
 /*Table structure for table `hms_doctor_availability` */
 
@@ -106,6 +112,8 @@ CREATE TABLE `hms_doctor_availability` (
 
 /*Data for the table `hms_doctor_availability` */
 
+insert  into `hms_doctor_availability`(`doctor_id`,`day_available`,`time_available`) values (1,'Sunday','15:45');
+
 /*Table structure for table `hms_drug` */
 
 DROP TABLE IF EXISTS `hms_drug`;
@@ -116,9 +124,11 @@ CREATE TABLE `hms_drug` (
   `qty` int(5) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_drug` */
+
+insert  into `hms_drug`(`id`,`drug_name`,`qty`,`price`) values (1,'ghghghgh',200,'588.00'),(2,'Penisileen',250,'600.00');
 
 /*Table structure for table `hms_employee_salary` */
 
@@ -167,7 +177,7 @@ CREATE TABLE `hms_inventory` (
 
 /*Data for the table `hms_inventory` */
 
-insert  into `hms_inventory`(`id`,`item_name`,`qty`,`created_date`,`created_user`,`updated_date`) values (1,'cbvcbvcbcv',6,'2018-08-28 15:14:38',5,'2018-08-28 03:14:44pm'),(2,'',0,'2018-08-28 15:14:47',5,NULL),(3,'',0,'2018-08-28 15:14:48',5,NULL),(4,'',0,'2018-08-28 15:14:48',5,NULL),(5,'',0,'2018-08-28 15:14:49',5,NULL),(6,'',0,'2018-08-28 15:15:41',5,NULL);
+insert  into `hms_inventory`(`id`,`item_name`,`qty`,`created_date`,`created_user`,`updated_date`) values (1,'Bad sheet',15,'2018-08-28 15:14:38',5,'2018-08-28 11:06:06pm'),(2,'Sample Test tube',15,'2018-08-28 15:14:47',5,'2018-08-28 11:05:36pm'),(3,'',0,'2018-08-28 15:14:48',5,NULL),(4,'',0,'2018-08-28 15:14:48',5,NULL),(5,'',0,'2018-08-28 15:14:49',5,NULL),(6,'',0,'2018-08-28 15:15:41',5,NULL);
 
 /*Table structure for table `hms_lab_test` */
 
@@ -178,12 +188,13 @@ CREATE TABLE `hms_lab_test` (
   `lab_test` varchar(50) DEFAULT NULL,
   `center_name` varchar(20) DEFAULT NULL,
   `description` text DEFAULT NULL,
+  `test_cost` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_lab_test` */
 
-insert  into `hms_lab_test`(`id`,`lab_test`,`center_name`,`description`) values (1,'hgjghjghjghj','Colombo','gdfgdfgfdgfdgsfgs gsdg'),(2,'fgfdgsgsfd gdfsgfdg ','Colombo','gfdsgfsdgfdsg fdgfdgfd gfdgdfg gfdgfdg');
+insert  into `hms_lab_test`(`id`,`lab_test`,`center_name`,`description`,`test_cost`) values (1,'hgjghjghjghj','Colombo','gdfgdfgfdgfdgsfgs gsdg',NULL),(2,'fgfdgsgsfd gdfsgfdg ','Colombo','gfdsgfsdgfdsg fdgfdgfd gfdgdfg gfdgfdg',NULL),(3,'Urine Test','Colombo','10:15 - 2:15 am \r\nBridsfdsf dsfdsfs',NULL),(4,'sample test','Colombo','sadsadasdas','2500.00');
 
 /*Table structure for table `hms_opd_appointment` */
 
@@ -221,9 +232,11 @@ CREATE TABLE `hms_patient` (
   `created_date` timestamp NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `NewIndex1_email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_patient` */
+
+insert  into `hms_patient`(`id`,`first_name`,`last_name`,`telephone`,`dob`,`email`,`pword`,`status_code`,`user_role`,`created_date`) values (1,'Kumarass','Gamage','0715288470','2018-08-01','ravinathdokk@gmail.com','7c4a8d09ca3762af61e59520943dc26494f8941b','ACTIVE','PATIENT','2018-08-29 13:56:03');
 
 /*Table structure for table `hms_purchase` */
 
@@ -239,9 +252,11 @@ CREATE TABLE `hms_purchase` (
   `created_user` int(5) DEFAULT NULL,
   `amount` double(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_purchase` */
+
+insert  into `hms_purchase`(`id`,`purchasing_item`,`qty`,`status_code`,`request_by`,`created_date`,`created_user`,`amount`) values (1,'sdsdsdsd',33,NULL,5,'2018-08-28 20:41:20',5,NULL),(2,'jhkjhjh',4545,'PENDING',8,'2018-08-28 21:39:36',8,NULL);
 
 /*Table structure for table `hms_specialist` */
 
@@ -280,7 +295,7 @@ CREATE TABLE `hms_user` (
 
 /*Data for the table `hms_user` */
 
-insert  into `hms_user`(`id`,`first_name`,`last_name`,`nic`,`pword`,`user_role`,`telephone`,`email`,`empno`,`status_code`,`created_date`,`created_user`) values (1,'kumara','pathirana','88','7c4a8d09ca3762af61e59520943dc26494f8941b','ADMIN','0111','admin@gmail.com','2255','ACTIVE','2018-07-27 21:20:34',1),(2,'Shalini','Tha','89','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','OPD','02244',NULL,'2266','ACTIVE','2018-08-02 12:01:43',1),(3,'Gaya','Siri','90','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','ACCOUNTANT',NULL,NULL,'2267','ACTIVE','2018-08-06 12:04:25',1),(5,'Nuwa','Per','92','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','LAB',NULL,NULL,'2269','ACTIVE','2018-08-06 12:06:43',1),(6,'KK','ii','93','7c4a8d09ca3762af61e59520943dc26494f8941b','TRANSPORT','011','ravi@gm','2270','ACTIVE','2018-08-06 16:03:16',1),(7,'kx','lklklxxs','94','7c4a8d09ca3762af61e59520943dc26494f8941b','PHARMACIST','055','dads@gm.l','6633','ACTIVE','2018-08-07 12:38:30',1),(8,'Rasintha','kjkjk','95','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','WARD','07155544','',NULL,'ACTIVE','2018-08-07 16:23:25',1),(9,'sdsadsad','asdsad','3323213','4b0de1d67de6a8a206e33e6eeddc0029fab3c748','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 15:56:04',NULL),(10,'sdsadsad','asdsad','452','4b0de1d67de6a8a206e33e6eeddc0029fab3c748','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 15:57:13',NULL),(11,'sdsds','sdsds','weweweV','87c208c74a690f34726af676c14717cd3f0a35ff','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 16:00:22',1),(12,'sdsds','sdsds','ss','87c208c74a690f34726af676c14717cd3f0a35ff','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 16:00:33',1),(13,'sdsds','sdsds','ee','87c208c74a690f34726af676c14717cd3f0a35ff','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 16:01:01',1),(16,'sdsds','sdsds','ees','256092013bb80044b9ec10600787cf31a5719c3e','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 16:02:43',1),(18,'sdsds','sdsds','ees4','5a1e92360a7fdc9b627895f7c25e9f7f9caae530','ADMIN','0715833470','ravinathdo@gmail.com','4455','ACTIVE','2018-08-13 16:04:14',1),(20,'sdfsdfsdfsdf','sdfsdfdsfds','f234234234','b54fa0d00b16130547c8b6603b4866a3ae02557e','ADMIN','0715833333','ravinathdo33@gmail.com','863333','ACTIVE','2018-08-13 16:13:14',1);
+insert  into `hms_user`(`id`,`first_name`,`last_name`,`nic`,`pword`,`user_role`,`telephone`,`email`,`empno`,`status_code`,`created_date`,`created_user`) values (1,'kumara','pathirana','88','7c4a8d09ca3762af61e59520943dc26494f8941b','ADMIN','0111','admin@gmail.com','2255','ACTIVE','2018-07-27 21:20:34',1),(2,'Shalinq','Tha','89','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','OPD','02244','','2266','ACTIVE','2018-08-02 12:01:43',1),(3,'Gayaxxs','Sirix','90','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','ACCOUNTANT','','','2267','ACTIVE','2018-08-06 12:04:25',1),(5,'Nuwax','Per','92','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','LAB','','','2269','ACTIVE','2018-08-06 12:06:43',1),(6,'KK','ii','93','7c4a8d09ca3762af61e59520943dc26494f8941b','TRANSPORT','011','ravi@gm','2270','ACTIVE','2018-08-06 16:03:16',1),(7,'kx','lklklxxs','94','7c4a8d09ca3762af61e59520943dc26494f8941b','PHARMACIST','055','dads@gm.l','6633','ACTIVE','2018-08-07 12:38:30',1),(8,'Rasintha','kjkjk','95','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8','WARD','07155544','',NULL,'ACTIVE','2018-08-07 16:23:25',1),(9,'sdsadsad','asdsad','3323213','4b0de1d67de6a8a206e33e6eeddc0029fab3c748','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 15:56:04',NULL),(10,'sdsadsad','asdsad','452','4b0de1d67de6a8a206e33e6eeddc0029fab3c748','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 15:57:13',NULL),(11,'sdsds','sdsds','weweweV','87c208c74a690f34726af676c14717cd3f0a35ff','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 16:00:22',1),(12,'sdsds','sdsds','ss','87c208c74a690f34726af676c14717cd3f0a35ff','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 16:00:33',1),(13,'sdsds','sdsds','ee','87c208c74a690f34726af676c14717cd3f0a35ff','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 16:01:01',1),(16,'sdsds','sdsds','ees','256092013bb80044b9ec10600787cf31a5719c3e','ADMIN',NULL,NULL,NULL,'ACTIVE','2018-08-13 16:02:43',1),(18,'sdsds','sdsds','ees4','5a1e92360a7fdc9b627895f7c25e9f7f9caae530','ADMIN','0715833470','ravinathdo@gmail.com','4455','ACTIVE','2018-08-13 16:04:14',1),(20,'sdfsdfsdfsdf','sdfsdfdsfds','f234234234','b54fa0d00b16130547c8b6603b4866a3ae02557e','ADMIN','0715833333','ravinathdo33@gmail.com','863333','ACTIVE','2018-08-13 16:13:14',1);
 
 /*Table structure for table `hms_user_role` */
 
@@ -305,9 +320,11 @@ CREATE TABLE `hms_vehicle` (
   `vehicle_number` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `NewIndex1` (`vehicle_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_vehicle` */
+
+insert  into `hms_vehicle`(`id`,`vehicle_number`) values (1,'8855-AS'),(2,'CAO-2365');
 
 /*Table structure for table `hms_vehicle_request` */
 
@@ -322,24 +339,27 @@ CREATE TABLE `hms_vehicle_request` (
   `created_time` timestamp NOT NULL DEFAULT current_timestamp(),
   `created_user` int(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_vehicle_request` */
+
+insert  into `hms_vehicle_request`(`id`,`request_by`,`comment`,`status_code`,`vehicle_id`,`created_time`,`created_user`) values (1,8,'plz get the ambulance','COMPLETE',2,'2018-08-28 23:10:14',8),(2,8,'plz get the negombo','PENDING',NULL,'2018-08-29 13:43:20',8),(3,8,'sample','PENDING',0,'2018-08-29 13:44:57',8),(4,8,NULL,'PENDING',0,'2018-08-29 13:45:47',8);
 
 /*Table structure for table `hms_ward` */
 
 DROP TABLE IF EXISTS `hms_ward`;
 
 CREATE TABLE `hms_ward` (
-  `ward_no` int(5) NOT NULL,
+  `ward_no` int(5) NOT NULL AUTO_INCREMENT,
   `ward_name` varchar(50) DEFAULT NULL,
   `doctor_incharge` int(5) DEFAULT NULL,
-  PRIMARY KEY (`ward_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`ward_no`),
+  UNIQUE KEY `NewIndex1` (`ward_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_ward` */
 
-insert  into `hms_ward`(`ward_no`,`ward_name`,`doctor_incharge`) values (1,'Child',0),(2,'Chest',0);
+insert  into `hms_ward`(`ward_no`,`ward_name`,`doctor_incharge`) values (1,'Child',1),(2,'Chest',0),(3,'asdasdsad',NULL),(4,'sadsadsad',1);
 
 /*Table structure for table `hms_ward_patient` */
 
@@ -352,12 +372,14 @@ CREATE TABLE `hms_ward_patient` (
   `comment` varchar(250) DEFAULT NULL,
   `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `created_user` int(5) DEFAULT NULL,
-  `status_code` varchar(20) DEFAULT NULL,
+  `status_code` varchar(20) DEFAULT 'ADMIT',
   PRIMARY KEY (`id`),
   UNIQUE KEY `NewIndex1` (`ward_id`,`patient_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Data for the table `hms_ward_patient` */
+
+insert  into `hms_ward_patient`(`id`,`ward_id`,`patient_id`,`comment`,`created_date`,`created_user`,`status_code`) values (1,1,1,'this is serous','2018-08-29 17:08:44',1,'ACCEPT');
 
 /*Table structure for table `hms_ward_staff` */
 
