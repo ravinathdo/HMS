514725da3f500a614c675f6901af2046ee8bb2f9
ravi <ravi@ravi-PC>
2018-08-30 08:57:51 +0530

    opd , accountant done , db 30082018

--- application/views/accountant/_tree_accountant.php
+++ application/views/accountant/_tree_accountant.php
@@ -3,7 +3,7 @@
     <li><tt><pre><a href="<?php echo base_url('Accountant_Controller/loadEmployeeSalary'); ?>"><i class="fas fa-users "></i>Employee Salary</a></pre></tt></li>
     <li><tt><pre><a href="<?php echo base_url('Accountant_Controller/loaddoctorSalary'); ?>"><i class="fas fa-user-md "></i> Doctor Salary</a></pre></tt></li>
     <li><tt><pre><a href="<?php echo base_url('Accountant_Controller/loadCostMaintain'); ?>"><i class="fas fa-money-bill-alt "></i> Cost</a></pre></tt></li>
-    <li><tt><pre><a href="<?php echo base_url('Transport_Controller/#'); ?>"></a> Reports</pre></tt></li>
+    <li><tt><pre><a href="<?php echo base_url('Transport_Controller/#'); ?>"><i class="fas fa-address-book"></i> Reports</a></pre></tt></li>
     <li><tt><pre><a href="<?php echo base_url('User_Controller/loadProfile'); ?>"><i class="fas fa-user-cog "></i> Profile</a></pre></tt></li>
 </ul>
 
